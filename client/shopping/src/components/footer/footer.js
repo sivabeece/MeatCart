2bc4464d6e1b423be80d3c27d17bb7d6fb6dbfd3
sivabeece <71876759+sivabeece@users.noreply.github.com>
2025-09-08 15:26:04 +0530

    Add files via upload

---
+++ client/shopping/src/components/footer/footer.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./footer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function FooterComponent() {
+  return (
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-cta pt-5 pb-5">
+          <div className="row">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="bi bi-geo-alt"></i>
+                <div className="cta-text">
+                  <h4>Find us</h4>
+                  <span>2/372-13A, Coimbatore, Tamil Nadu</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="bi bi-telephone"></i>
+                <div className="cta-text">
+                  <h4>Call us</h4>
+                  <span>8495944440</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="bi bi-envelope"></i>
+                <div className="cta-text">
+                  <h4>Mail us</h4>
+                  <span>talk@meatcart.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>
+                  Copyright &copy; <a href="">newcosoft</a>{" "}
+                  {new Date().getFullYear()}, All Right Reserved
+                </p>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy</a>
+                  </li>
+                  <li>
+                    <a href="#">Policy</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterComponent;
